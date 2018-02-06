54ac8e8a0d2b1dbb866936633097affdc3d6b3b2
jalili.developer@gmail.com <jalili.developer@gmail.com>
2018-02-05 16:32:30 -0800

    asdsa

--- INewGN/Models/dbINewsGn.Designer.cs
+++ INewGN/Models/dbINewsGn.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\ProjectAM\INewGN\INewGN\Models\dbINewsGn.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\ProjectAM\INewsGNProjectServerB\INewGN\Models\dbINewsGn.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
